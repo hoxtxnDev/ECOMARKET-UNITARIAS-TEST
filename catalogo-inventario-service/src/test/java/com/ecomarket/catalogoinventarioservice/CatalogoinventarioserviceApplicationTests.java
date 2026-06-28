5a2a0eab61b154e272ee735c67c60ae1303dbf46
Hans <hwnss.w@gmail.com>
2026-06-28 00:58:25 -0400

    FIX: CatalogoinventarioserviceApplication con cobertura al 100%

--- catalogo-inventario-service/src/test/java/com/ecomarket/catalogoinventarioservice/CatalogoinventarioserviceApplicationTests.java
+++ catalogo-inventario-service/src/test/java/com/ecomarket/catalogoinventarioservice/CatalogoinventarioserviceApplicationTests.java
@@ -1,18 +1,26 @@
 package com.ecomarket.catalogoinventarioservice;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CatalogoinventarioserviceApplicationTests {
 
 	@Test
+	@DisplayName("El contexto de Spring Boot carga correctamente")
 	void contextLoads() {
 	}
 
 	@Test
-	void mainStartsApplication() {
-		CatalogoinventarioserviceApplication.main(new String[]{});
+	@DisplayName("El método main se ejecuta correctamente")
+	void mainTest() {
+		CatalogoinventarioserviceApplication.main(new String[] {
+			"--spring.main.web-application-type=none",
+			"--spring.profiles.active=test"
+		});
 	}
 
 }
